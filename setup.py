65b9f197d5554ae8b3a94f4437ec7506e3439e18
Brewster Malevich <sbmalev@gmail.com>
2017-11-08 09:50:57 -0700

    Added package data to setup script

--- setup.py
+++ setup.py
@@ -64,7 +64,9 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'pytest'],
+    install_requires=['numpy', 'scipy'],
+    
+    package_data={'erebus': ['benthic_stacks/*.csv']},
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
