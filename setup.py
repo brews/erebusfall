bc985fe945e7d7948c14e79f309bae7ac689cc4d
Brewster Malevich <sbmalev@gmail.com>
2018-03-01 14:05:17 -0700

    Release v0.0.1

--- setup.py
+++ setup.py
@@ -14,45 +14,31 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='erebus',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.1',
 
     description='Ice-volume correction to marine-isotope proxy records in Python',
 
-    # The project's main homepage.
     url='https://github.com/brews/erebus',
 
-    # Author details
     author='S. Brewster Malevich',
     author_email='malevich@email.arizona.edu',
 
-    # Choose your license
     license='GPLv3',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 5 - Production/Stable',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
     ],
 
-    # What does your project relate to?
     keywords='marine isotope paleoclimate proxy deuterium d18o',
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs']),
     install_requires=['numpy', 'scipy'],
     tests_require=['pytest'],
